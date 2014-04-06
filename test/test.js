472b48b93daefbdfb001794dff2593a0f9591036
mk-pmb <mk-pmb@users.noreply.github.com>
2014-04-06 15:16:34 +0200

    Extend previously identical test names.
    I had to first replace repeating string parts with named constants
    in order to understand the differences in tests.

--- test/test.js
+++ test/test.js
@@ -3,6 +3,11 @@ const file = require('fs-utils');
 const strip = require('../');
 const testDir = file.dirname(module.filename);
 
+const blCmt = '/* some block comment */';
+const lnCmt = '// some line comment';
+const barFuncHead = 'var bar = function(';
+const blExclamCmt = '/*! this is a comment */';
+
 
 describe('empty:', function () {
   it('should not throw an error at an empty string.', function () {
@@ -48,67 +53,84 @@ describe('all except banners:', function () {
 });
 
 describe('all comments:', function () {
-  it('should strip all comments.', function () {
-    var actual = strip('// this is a line comment\nvar bar = function(/*this is a comment*/) {return;};');
-    var expected = '\nvar bar = function() {return;};';
+  it('should strip all comments, line and block.', function () {
+    var actual = strip(lnCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};\n' +
+      lnCmt);
+    var expected = '\n' + barFuncHead + ') {return;};\n';
     expect(actual).to.eql(expected);
   });
 
-  it('should strip all comments.', function () {
-    var actual = strip('/* this is a comment */\n//this is a comment\nvar bar = function(/*this is a comment*/) {return;};');
-    var expected = '\n\nvar bar = function() {return;};';
+  it('should strip all comments, even block c. before line c..', function () {
+    var actual = strip(blCmt + '\n' +
+      lnCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};');
+    var expected = '\n\n' + barFuncHead + ') {return;};';
     expect(actual).to.eql(expected);
   });
 
   it('should strip all comments including one with an exclamation at the beginning.', function () {
-    var actual = strip('/*! this is a comment */\n//this is a comment\nvar bar = function(/*this is a comment*/) {return;};');
-    var expected = '\n\nvar bar = function() {return;};';
+    var actual = strip(blExclamCmt + '\n' +
+      lnCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};');
+    var expected = '\n\n' + barFuncHead + ') {return;};';
     expect(actual).to.eql(expected);
   });
 
-  it('should strip all comments.', function () {
-    var actual = strip('/* this is a comment */\n//this is a comment\nvar bar = function(/*this is a comment*/) {return;};\n//this is a line comment');
-    var expected = '\n\nvar bar = function() {return;};\n';
+  it('should strip all comments, even line c. in last line w/o newline.', function () {
+    var actual = strip(blCmt + '\n' +
+      lnCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};\n'
+        + lnCmt);
+    var expected = '\n\n' + barFuncHead + ') {return;};\n';
     expect(actual).to.eql(expected);
   });
 });
 
 describe('block comments:', function () {
   it('should strip block comments from a function.', function () {
-    var actual = strip.block('var bar = function(/*this is a comment*/) {return;};');
-    var expected = 'var bar = function() {return;};';
+    var actual = strip.block('' + barFuncHead + blCmt + ') {return;};');
+    var expected = '' + barFuncHead + ') {return;};';
     expect(actual).to.eql(expected);
   });
 
   it('should strip block comments before and from a function.', function () {
-    var actual = strip.block('/* this is a comment */\nvar bar = function(/*this is a comment*/) {return;};');
-    var expected = '\nvar bar = function() {return;};';
+    var actual = strip.block(blCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};');
+    var expected = '\n' + barFuncHead + ') {return;};';
     expect(actual).to.eql(expected);
   });
 
   it('should strip block comments before, after and from a function.', function () {
-    var actual = strip.block('/* this is a comment */var bar = function(/*this is a comment*/) {return;};\n/* this is a comment*/');
-    var expected = 'var bar = function() {return;};\n';
+    var actual = strip.block(blCmt + barFuncHead + blCmt + ') {return;};\n' +
+      blCmt);
+    var expected = '' + barFuncHead + ') {return;};\n';
     expect(actual).to.eql(expected);
   });
 });
 
 describe('line comments:', function () {
-  it('should strip line comments.', function () {
-    var actual = strip.line('// this is a line comment\nvar bar = function(/*this is a comment*/) {return;};');
-    var expected = '\nvar bar = function(/*this is a comment*/) {return;};';
+  it('should strip line comments, keep block comments.', function () {
+    var actual = strip.line(lnCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};');
+    var expected = '\n' + barFuncHead + blCmt + ') {return;};';
     expect(actual).to.eql(expected);
   });
 
   it('should strip line comments before a function, and not block comments.', function () {
-    var actual = strip.line('/* this is a comment */\n//this is a comment\nvar bar = function(/*this is a comment*/) {return;};');
-    var expected = '/* this is a comment */\n\nvar bar = function(/*this is a comment*/) {return;};';
+    var actual = strip.line(blCmt + '\n' +
+      lnCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};');
+    var expected = blCmt + '\n\n' + barFuncHead + blCmt + ') {return;};';
     expect(actual).to.eql(expected);
   });
 
   it('should strip line comments before and after a function, and not block comments.', function () {
-    var actual = strip.line('/* this is a comment */\n//this is a comment\nvar bar = function(/*this is a comment*/) {return;};\n//this is a line comment');
-    var expected = '/* this is a comment */\n\nvar bar = function(/*this is a comment*/) {return;};\n';
+    var actual = strip.line(blCmt + '\n' +
+      lnCmt + '\n' +
+      barFuncHead + blCmt + ') {return;};\n'
+        + lnCmt);
+    var expected = blCmt + '\n\n' + barFuncHead + blCmt + ') {return;};\n';
     expect(actual).to.eql(expected);
   });
 });
