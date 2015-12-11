5aaf96e6f80e0f91f04b34863affcd3ef97d2780
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-11 18:21:36 -0500

    adds example

---
+++ example.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var fs = require('fs');
+var strip = require('./');
+
+var all = fs.readFileSync('test/fixtures/strip-all.js', 'utf8');
+var str = fs.readFileSync('test/fixtures/banner.js', 'utf8');
+
+// console.log(strip.first(all))
+// console.log(strip.extract(all));
+// console.log(strip.block(all));
+// console.log(strip.line(all));
+console.log(strip(all));
