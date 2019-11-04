a5af90cd5d0b5383b8346d2c319d8a94935c8ea8
Jon Schlinkert <jon.schlinkert@sellside.com>
2019-11-04 09:26:06 -0500

    update docs, coverage

--- examples/support/helpers.js
+++ examples/support/helpers.js
@@ -1,4 +1,13 @@
+const languages = require('../../lib/languages');
 
-exports.strip = function(str) {
-  return str.split('*\\/').join('*/');
+exports.strip = str => str.split('*\\/').join('*/');
+
+// this is used by the readme template to generate the list of languages.
+exports.languages = () => {
+  const omit = new Set(['js', 'ts']);
+  const keys = Object.keys(languages)
+    .sort((a, b) => a.localeCompare(b))
+    .filter(key => !omit.has(key));
+
+  return `\n- ${keys.join('\n- ')}`;
 };
